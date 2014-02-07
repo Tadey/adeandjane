63bbe80e62e655f27276052c8bcc5ff8d8f98ac7
Ade Peace <adrianpeace@gmail.com>
2014-02-07 12:33:07 +0000

    Added graphic designed version of content placeholder. Intention to let visitor's know that site is up and content will be updated soon

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-
+<head>
+	<title>Ade and Jane's Wedding Site</title>
+	<link href = "css/styles.css" type = "text/css" rel = "stylesheet" />
+</head>
 <body>
-	<h1>Ade and Jane's Wedding!</h1>
+	<div id = 'page_area'>
+		<img src = "images/homepage.png"
+			class = "homepage align-centre" alt = "Homepage" />
+	</div> <!-- Page Area -->
+	 
+	
 	
 	
 </body>
